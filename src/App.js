a8d4384ba906482251e01204268cef9cc83dac33
Pattem-Chaitanya <chaitanya.pattem@ftlabs.io>
2024-07-14 16:38:32 +0530

    made it functional

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import Layout from "./component/Layout";
 
 const App = () => {
+  const [darkMode, setDarkMode] = useState(false);
   const headerTags = {
     title: "Home - React Boiler plate",
     keywords: "home",
     titleTemplate: "Home - React Boiler plate",
     description: "Home of this react boiler plate",
   };
-  return <Layout headerTags={headerTags}>App</Layout>;
+
+  return (
+    <Layout headerTags={headerTags}>
+      <div className="container">
+        <h1>App</h1>
+        <button
+          onClick={() => {
+            setDarkMode(!darkMode);
+            if (darkMode) {
+              document.body.setAttribute("data-theme", "dark");
+            } else {
+              document.body.removeAttribute("data-theme");
+            }
+          }}
+        >
+          Change to {darkMode ? "light" : "dark"} mode
+        </button>
+      </div>
+    </Layout>
+  );
 };
 
 export default App;
